bca4bb02279411d49c657fa8f592a7579356251c
Mariusz Libera <mariusz.libera@gmail.com>
2013-09-23 19:48:08 +0200

    upd

--- python_startup.py
+++ python_startup.py
@@ -1,17 +1,6 @@
-import atexit
-import os
 import readline
 import rlcompleter
 
-historyPath = os.path.expanduser("~/.cache/python_history")
 readline.parse_and_bind('tab: complete')
 
-def save_history(historyPath=historyPath):
-    import readline
-    readline.write_history_file(historyPath)
-
-if os.path.exists(historyPath):
-    readline.read_history_file(historyPath)
-
-atexit.register(save_history)
-del os, atexit, readline, rlcompleter, save_history, historyPath
+del readline, rlcompleter
